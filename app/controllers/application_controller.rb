fc0d5489e4509ca2dc1a57c6c8fe52f82a6be5a9
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2025-10-17 13:06:22 +0500

    Cleaned up duplicated logic in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  layout :set_layout
   # Redirect admin after login
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       admin_dashboard_path  # <-- Replace with your actual dashboard route
     else
       super
+    end
+  end
+
+  private
+
+  def set_layout
+    if request.path.start_with?('/admin')
+      'admin'
+    else
+      'application'
     end
   end
 end
