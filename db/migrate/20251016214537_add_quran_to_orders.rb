1bf1f347074a0586b8f539da1c3c32240e7b485c
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2025-10-24 15:35:59 +0500

    implemented discord alert integration on new order

--- db/migrate/20251016214537_add_quran_to_orders.rb
+++ db/migrate/20251016214537_add_quran_to_orders.rb
@@ -2,7 +2,7 @@ class AddQuranToOrders < ActiveRecord::Migration[7.2]
   def change
     # Add the polymorphic Quran association to orders
     add_column :orders, :quran_type, :string
-    add_column :orders, :quran_id, :bigint
+    # add_column :orders, :quran_id, :bigint
 
     # Optional: Add index for performance
     add_index :orders, [:quran_type, :quran_id]
--- db/schema.rb
+++ db/schema.rb
@@ -93,9 +93,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_19_035520) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "quran_type"
     t.bigint "quran_id"
     t.string "translation"
+    t.string "quran_type"
     t.index ["quran_type", "quran_id"], name: "index_orders_on_quran_type_and_quran_id"
   end
 
